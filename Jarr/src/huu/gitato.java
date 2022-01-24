a78b12c492cd3966736456cfdb938a7c5adc717f
topolr <topolr@192.168.56.1>
2022-01-24 08:53:45 +0100

    sys

---
+++ Jarr/src/huu/gitato.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package huu;
+
+/**
+ * @author topolr
+ *
+ */
+public class gitato {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("dobrrrrý den ho");
+
+	}
+
+}
